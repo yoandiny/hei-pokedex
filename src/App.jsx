74505ad13e8e17c02bbc13a72e14fbc5ad91e3f9
Yoan <yoandiny14@gmail.com>
2025-08-02 20:38:06 +0300

    fixed Navbar integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import { Route, BrowserRouter, Routes } from 'react-router-dom'
 import Homepage from './pages/Homepage'
+import { PokemonProfile } from './pages/PokemonProfile'
+import Navbar from './component/Navbar'
 
 
 function App() {
@@ -10,7 +12,10 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
+          <Route element={<Navbar />}>
           <Route path="/" element={<Homepage />} />
+          <Route path='/profile' element={<PokemonProfile />} />
+          </Route>
         </Routes>
        </BrowserRouter>
       
