e379dd5c02a176776ef7cc92efa8814f7763fe37
Rindra <hei.rindra.2@gmail.com>
2025-08-05 19:01:35 +0300

    Add input search with button search

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -4,15 +4,24 @@ import pokedex_logo from "../assets/images/pokedex-removebg-preview.png";
 export default function Navbar() {
   return (
     <>
-    <nav className="flex justify-center items-center bg-white py-2">
-      <div className="flex items-center space-x-4">
-       <Link to="/"><img src={pokedex_logo} alt="Logo Pokémon" className="w-50 py-3"/></Link>
+        
+      <div className="flex flex-col justify-center  items-center bg-white py-2">
+         <Link to="/"><img src={pokedex_logo} alt="Logo Pokémon" className="w-50 py-3"/></Link>
+        <div className="flex md:w-3xl lg:w-6xl items-center border-2 rounded-md overflow-hidden">
+          <input
+              type="search"
+              className="px-4 py-2 w-full focus:outline-none"
+              placeholder="Search name, type, number "
+          />
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4">
+            <i class="fa fa-search"></i>
+          </button>
+        </div>
       </div>
-    </nav>
-
-    <main>
-      <Outlet />
-    </main>
+  
+      <main>
+        <Outlet />
+      </main>
     
     </>
   );
