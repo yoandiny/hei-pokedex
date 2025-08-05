661dcb8f6fd1d6c8d5e5f0ee8a8e307cdb739d64
Rindra <hei.rindra.2@gmail.com>
2025-08-05 20:05:25 +0300

    add compement navbar

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,13 @@
 import PokeList from '../component/PokeList'
+import {useState} from "react";
+import Navbar from "../component/Navbar.jsx";
 
 const Homepage = () => {
+    const [searchTerm, setSearchTerm] = useState("")
   return (
     <div>
-      
-      <PokeList />
+      <Navbar onSearch={setSearchTerm}/>
+      <PokeList searchTerm={searchTerm}/>
     </div>
   )
 }
